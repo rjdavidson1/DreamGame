75ed243f48d6830e14b2eb9b6417f687119ca009
Thatalent <jonathan.hudson2012@gmail.com>
2016-01-30 15:15:10 -0500

    Jonathan-Worked on Base Player script

--- Assets/Scripts/BasePlayer.cs
+++ Assets/Scripts/BasePlayer.cs
@@ -3,13 +3,30 @@ using System.Collections;
 
 public class BasePlayer : MonoBehaviour {
 
+    
 	// Use this for initialization
 	void Start () {
-	
+        Acceleration = 0.05f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+        if (Input.GetButton("Run"))
+        {
+            if (Speed < 0.3)
+            {
+                Speed = Speed + Acceleration;
+            }
+            Player.transform.localPosition = new Vector3((Player.transform.localPosition.x + Speed), Player.transform.localPosition.y, Player.transform.localPosition.z);
+        }
+
 	}
+
+    public GameObject Player { get; set; }
+    public float Acceleration { get; set; }
+    public float Speed { get; set; }
+    public float Gravity { get; set; }
+    public bool Dead { get; set; }
+
 }
