cab08596411248474ded6dd821c21069e2394e63
Thatalent <jonathan.hudson2012@gmail.com>
2016-01-31 12:55:19 -0500

    Jonathan-Added fixes to gameover position

---
+++ Assets/GameOver.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOver : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.localPosition = new Vector3(GameObject.FindGameObjectWithTag("MainCamera").transform.localPosition.x, GameObject.FindGameObjectWithTag("MainCamera").transform.localPosition.y);
+	}
+}
