f36daa7b316396e7599fcd9896916586c36343d4
rjdavidson1 <rjdavidson@smcm.edu>
2016-01-30 23:12:57 -0500

    Rose - some more stuff

---
+++ Assets/Scripts/Menu/CreditsScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditsScript : MonoBehaviour {
+    private GameObject credits;
+	// Use this for initialization
+	void Start () {
+        credits = GameObject.Find("Credits_Button");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(credits.)
+	}
+}
