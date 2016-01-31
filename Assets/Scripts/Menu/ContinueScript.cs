d3b0ce74a313d59864045e0b94d01032f1f8efbe
rjdavidson1 <rjdavidson@smcm.edu>
2016-01-30 19:26:30 -0500

    Rose - Added more menu items

---
+++ Assets/Scripts/Menu/ContinueScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ContinueScript : MonoBehaviour {
+    private GameObject mouseoverL;
+    private GameObject mouseoverR;
+	// Use this for initialization
+	void Start () {
+        mouseoverL = GameObject.Find("MouseOver_ButtonC1");
+        mouseoverR = GameObject.Find("MouseOver_ButtonC2");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
