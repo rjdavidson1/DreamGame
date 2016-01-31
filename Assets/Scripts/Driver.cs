fbaaf6e5d3565f3717898ea9ab2c66a4dc5b03c2
Thatalent <jonathan.hudson2012@gmail.com>
2016-01-30 20:11:48 -0500

    Jonathan- added Driver class; Chnaged location of some scripts.

---
+++ Assets/Scripts/Driver.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Driver  {
+
+	public Driver()
+    {
+        CurrentLvl = 0;
+        LongestTime = 0f;
+        PlayerLives = 3;
+    }
+
+    public Driver(int currentLvl, float longestTime, int playerLives)
+    {
+        CurrentLvl = currentLvl;
+        LongestTime = longestTime;
+        PlayerLives = playerLives;   
+
+    }
+
+    public void levelLoad()
+    {
+        switch (CurrentLvl)
+        {
+            case 0: SceneManager.LoadScene("Level0");
+                break;
+            case 1: SceneManager.LoadScene("Level1");
+                break;
+            case 2: SceneManager.LoadScene("Level2");
+                break;
+            default: SceneManager.LoadScene("Level0");
+                break;
+        }
+    }
+    static public int CurrentLvl { get; set; }
+    static public float LongestTime { get; set; }
+    static public int PlayerLives { get; set; }
+
+}
